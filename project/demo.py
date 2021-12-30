254ac7d5a929e68c44aeee35ab26c9186fbcee4f
delldu <18588220928@163.com>
2021-12-30 09:48:28 +0800

    Update

--- project/demo.py
+++ project/demo.py
@@ -12,9 +12,10 @@
 import segment_former
 
 if __name__ == "__main__":
-    # segment_former.image_predict("images/*.png", "output")
-    # segment_former.image_client("PAI", "images/*.png", "output")
-    # segment_former.image_server("PAI")
+    # segment_former.image_predict("images/*.png", "output/predict")
 
-    segment_former.video_client("PAI", "/home/dell/tennis.mp4", "output/tennis.mp4")
-    segment_former.video_server("PAI")
+    segment_former.image_client("PAI", "images/*.png", "output/client")
+    segment_former.image_server("PAI")
+
+    # segment_former.video_client("PAI", "/home/dell/tennis.mp4", "output/tennis.mp4")
+    # segment_former.video_server("PAI")
